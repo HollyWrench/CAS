91c6e3f8f22d8e207e286394645f4ba6bfbb5be8
Susheel Shivaprasad <susheelshivaprasad@gmail.com>
2021-08-31 15:24:41 +0100

    routes for profile page, events and booking

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :committee_position, inclusion: { in: ["President", "Treasurer", "Secretary", "Events-Manager", "Social-Secretary", "Publicity-Officer", "Content-Creator", ""],
-    message: "Please select from: President, Treasurer, Secretary, Events-Manager, Social-Secretary, Publicity-Officer, Content-Creator" }
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true
+  # validates :committee_position, inclusion: { in: ["President", "Treasurer", "Secretary", "Events-Manager", "Social-Secretary", "Publicity-Officer", "Content-Creator", ""],
+  #   message: "Please select from: President, Treasurer, Secretary, Events-Manager, Social-Secretary, Publicity-Officer, Content-Creator" }
   validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
 end
