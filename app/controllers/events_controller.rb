13f414eb174ebf530dbc9d582c5771bd673da172
Susheel Shivaprasad <susheelshivaprasad@gmail.com>
2021-08-31 15:42:34 +0100

    added all actions for events controller

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,45 @@
+class EventsController < ApplicationController
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.user = current_user
+    if @event.save
+      # redirect_to events_path, notice: "Thanks for your suggestion (:"
+    else
+      render :new
+    end
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    @event.update!(event_params)
+    redirect_to @event
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to events_path
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:location, :capacity, :category, :name, :description, :photo)
+  end
+end
