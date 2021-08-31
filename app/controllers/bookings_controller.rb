eceec0f8cbff0f6e9060be67c5d8d5b76a9d3aa3
Holly Wrench <holly.wrench@gmail.com>
2021-08-31 15:35:30 +0100

    bookings controller done

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,34 @@
+class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+    @event = Event.find(params[:event_id])
+    @user = current_user
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @event = Event.find(params[:event_id])
+    @user = current_user
+    @booking.event = @event
+    @booking.user = @user
+    if @booking.save
+      redirect_to bookings_path, notice: "Your booking was successful."
+    else
+      render :new
+    end
+  end
+
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:user_id, :event_id)
+  end
+end
