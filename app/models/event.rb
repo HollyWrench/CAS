5d87e06a7e950df07475d1f6364470a9b19f411f
Susheel Shivaprasad <susheelshivaprasad@gmail.com>
2021-08-31 12:55:07 +0100

    validations for events and users created and tested

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,7 @@
 class Event < ApplicationRecord
+  validates :name, presence: true
+  validates :location, presence: true
+  validates :price, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+  validates :description, presence: true
 end
