705c8ba0814b6383432f646db93b35b1dfa5f955
Holly Wrench <holly.wrench@gmail.com>
2021-08-31 16:40:14 +0100

    finished controller for feed items, hopefully the .is_admin? will work

--- app/controllers/feed_items_controller.rb
+++ app/controllers/feed_items_controller.rb
@@ -1,13 +1,6 @@
 class FeedItemsController < ApplicationController
 
-  before_action :is_admin?
-
-  def is_admin?
-    committee_positions = ["President", "Treasurer", "Secretary", "Events-Manager", "Social-Secretary", "Publicity-Officer", "Content-Creator"]
-    redirect_to feed_items_path unless committee_positions.include(current_user.committee_position)
-  end
-
-
+  before_action :is_admin?, except: :index
 
   def index
     @feed_items = FeedItem.all
@@ -26,9 +19,9 @@ class FeedItemsController < ApplicationController
   end
 
   def create
-    @feed_item = FeedItem.new(event_params)
+    @feed_item = FeedItem.new(feed_items_params)
     if @feed_item.save
-      # redirect_to feed_items_path, notice: "Thanks for your suggestion (:"
+      redirect_to feed_items_path, notice: "Item created"
     else
       render :new
     end
@@ -37,12 +30,23 @@ class FeedItemsController < ApplicationController
   def update
     @feed_item = FeedItem.find(params[:id])
     @feed_item.update!(event_params)
-    redirect_to @feed_items
+    redirect_to @feed_items, notice: "Item updated"
   end
 
   def destroy
     @feed_item = FeedItem.find(params[:id])
     @feed_item.destroy
-    redirect_to feed_items_path
+    redirect_to feed_items_path, notice: "Item deleted"
+  end
+
+  private
+
+  def is_admin?
+    committee_positions = ["President", "Treasurer", "Secretary", "Events-Manager", "Social-Secretary", "Publicity-Officer", "Content-Creator"]
+    redirect_to feed_items_path unless committee_positions.include?(current_user.committee_position)
+  end
+
+  def feed_items_params
+    params.require(:feed_item).permit(:description, :url)
   end
 end
