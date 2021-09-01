89a4c03f5545ec41b6fd155d39f242ef3f432133
omer akhtar <oakhtar11@gmail.com>
2021-09-01 12:37:01 +0100

    cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get '/about', to: 'pages#about'
   get '/profile', to: 'pages#profile'
   resources :events do
     resources :bookings, only: %i[new create index destroy]
   end
   resources :bookings, only: :index
+  resources :feed_items
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
