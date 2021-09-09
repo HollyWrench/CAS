af449cd8bf13043786c2858e6173504ce0529b09
Holly Wrench <holly.wrench@gmail.com>
2021-09-09 16:55:05 +0100

    import mapbox stylesheet

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,6 +24,7 @@ require("channels")
 
 // External imports
 import "bootstrap";
+import "controllers";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
@@ -50,5 +51,3 @@ document.addEventListener('turbolinks:load', () => {
     fadeIn();
   }
 });
-
-import "controllers"
